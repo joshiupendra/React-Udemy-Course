c67b04a42c74bf58166e68a750000be1c137193f
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-11 15:12:48 +0530

    Updated app structure

--- react-starting-project/src/App.jsx
+++ react-starting-project/src/App.jsx
@@ -1,30 +1,25 @@
-import reactImg from './assets/react-core-concepts.png';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (<header>
-    <img src={reactImg} alt="Stylized atom" />
-    <h1>React Essentials</h1>
-    <p>
-      {description} React concepts you will need for almost any app you are
-      going to build!
-    </p>
-  </header>);
-}
+import { CORE_CONCEPTS } from './data.js';
+import Header from './components/Header/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title={CORE_CONCEPTS[0].title} 
+              description={CORE_CONCEPTS[0].description}  
+              image={CORE_CONCEPTS[0].image}  />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} /> 
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        
       </main>
     </div>
   );
