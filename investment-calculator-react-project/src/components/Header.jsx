2ccfe7a71bfaddcf82f282b5c4dc93c2820822d8
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-13 08:37:32 +0530

    React Investment Calculator

---
+++ investment-calculator-react-project/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import image from '../assets/investment-calculator-logo.png';
+
+export default function Header() {
+  return (
+    <div id="header">
+      <img src={image} alt="Investment Calculator Logo" />
+      <h1>React Investment Calculator</h1>
+    </div>
+  );
+}
