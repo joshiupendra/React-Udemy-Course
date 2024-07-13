69ce5250a302c94d0dd91d2e0663766924e414ed
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-13 09:44:08 +0530

    Completed React Investment Calculator Project

--- investment-calculator-react-project/src/App.jsx
+++ investment-calculator-react-project/src/App.jsx
@@ -1,11 +1,33 @@
+import { useState } from 'react';
 import Header from "./components/Header.jsx"
 import UserInput from "./components/UserInput.jsx"
+import Results from './components/Results.jsx';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue
+      };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {!inputIsValid && <p>Please Enter Duration greater than 0.</p>}
+      {inputIsValid && <Results userInput={userInput} />}
     </>
   )
 }
