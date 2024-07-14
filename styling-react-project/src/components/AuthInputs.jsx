57e1424c567f04fc5f6ef25d0d6fcce2ae5b8088
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-14 12:47:38 +0530

    updated Tailwind CSS

--- styling-react-project/src/components/AuthInputs.jsx
+++ styling-react-project/src/components/AuthInputs.jsx
@@ -31,17 +31,17 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800">
+      <div className='flex flex-col gap-2 mb-6'> 
         <Input label='Email' invalid={emailNotValid} onChange={(event) =>
               handleInputChange('email', event.target.value)
             }/>
         <Input label='Password' invalid={passwordNotValid} onChange={(event) =>
               handleInputChange('password', event.target.value)
             } />
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button /*className='button'*/ onClick={handleLogin}>Sign In</Button>
--- styling-react-project/src/components/Input.jsx
+++ styling-react-project/src/components/Input.jsx
@@ -22,10 +22,21 @@ const Input = styled.input`
 `;
 
 export default function CustomInput({label, invalid, ...props}) {
+  let labelClasses = "block mb-2 text-xs font-bold tracking-wide uppercase ";
+  let inputClasses = "w-full px-3 py-2 leading-tight border rounded shadow ";
+
+  if (invalid) {
+    labelClasses += "text-red-400";
+    inputClasses += "text-red-500 bg-red-100 border-red-300";
+  } else {
+    labelClasses += "text-stone-300";
+    inputClasses += "text-gray-700 bg-stone-300";
+  }
+
   return (
     <p>
-      <Label $invalid={invalid}>{label}</Label>
-      <Input $invalid={invalid} {...props} />
+      <label className={labelClasses} >{label}</label>
+      <input className={inputClasses} {...props} />
     </p>
   );
 }
