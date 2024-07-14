b8f29d709b597109085e7d1575eef2d0cd56ef9f
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-14 11:38:28 +0530

    Added Tailwind CSS in react project

--- styling-react-project/src/components/AuthInputs.jsx
+++ styling-react-project/src/components/AuthInputs.jsx
@@ -1,32 +1,14 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
+import Button from './Button.jsx';
+import Input from './Input.jsx';
 
 const ControlContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
-const Label = styled.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${(props) => props.$invalid ? '#f87171' : '#6b7280' };
-`
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({$invalid}) => $invalid ? '#fed2d2' : '#d1d5db'};
-  color: ${({$invalid}) => $invalid ? '#ef4444' : '#374151'};
-  border: 1px solid ${({$invalid}) => $invalid ? '#f73f3f' : 'transparent'};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -51,30 +33,18 @@ export default function AuthInputs() {
   return (
     <div id="auth-inputs">
       <ControlContainer>
-        <p>
-          <Label $invalid={emailNotValid} /*className={`label ${emailNotValid ? 'invalid' : ''}`} */>Email</Label>
-          <Input $invalid={emailNotValid}
-            type="email"
-            /*className={emailNotValid ? 'invalid' : undefined}*/
-            onChange={(event) => handleInputChange('email', event.target.value)}
-          />
-        </p>
-        <p>
-          <Label $invalid={passwordNotValid} /*className={`label ${passwordNotValid ? 'invalid' : ''}`}*/ >Password</Label>
-          <Input $invalid={passwordNotValid}
-            type="password"
-            /*className={passwordNotValid ? 'invalid' : undefined}*/
-            onChange={(event) =>
+        <Input label='Email' invalid={emailNotValid} onChange={(event) =>
+              handleInputChange('email', event.target.value)
+            }/>
+        <Input label='Password' invalid={passwordNotValid} onChange={(event) =>
               handleInputChange('password', event.target.value)
-            }
-          />
-        </p>
+            } />
       </ControlContainer>
       <div className="actions">
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button /*className='button'*/ onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
--- styling-react-project/src/components/Header.jsx
+++ styling-react-project/src/components/Header.jsx
@@ -42,14 +42,18 @@ const StyledHeader = styled.header`
     & h1 {
       font-size: 2.25rem;
     }
-`
+`;
 
 export default function Header() {
   return (
-    <StyledHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p /*className={classes.paragraph}*/>A community of artists and art-lovers.</p>
-    </StyledHeader>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44 object-contain" />
+      <h1 className="text-4xl font-semibold tracking-widest text-center uppercase text-amber-800 font-title">
+        ReactArt
+      </h1>
+      <p className="text-stone-500" /*className={classes.paragraph}*/>
+        A community of artists and art-lovers.
+      </p>
+    </header>
   );
 }
