8fd8abc4dbdabee1dee9cc20d11b8a4d082209c3
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-15 14:38:23 +0530

    Used Portal

--- react-refs-portal-project/src/components/ResultModal.jsx
+++ react-refs-portal-project/src/components/ResultModal.jsx
@@ -1,10 +1,12 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { createPortal } from 'react-dom';
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
   const dialog = useRef();
 
   const userLost = remainingTime <=0;
   const formattedRemaingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
@@ -14,17 +16,17 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
     }
   });
 
-  return (
-    <dialog ref={dialog} className="result-modal">
-      {userLost && <h2>
-        You Lost!
-      </h2>}
+  return createPortal(
+    <dialog ref={dialog} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You Lost!</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped the time with <strong>{formattedRemaingTime} seconds left.</strong></p>
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>        
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 })
 
