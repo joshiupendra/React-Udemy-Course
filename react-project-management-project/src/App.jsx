6ca5e0cd310c490a048e5e499d7dcef960ec4f4c
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-16 14:55:50 +0530

    Project add functionality added

--- react-project-management-project/src/App.jsx
+++ react-project-management-project/src/App.jsx
@@ -1,11 +1,51 @@
+import { useState } from 'react';
 import NewProject from "./components/NewProject.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    });
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- react-project-management-project/src/components/NewProject.jsx
+++ react-project-management-project/src/components/NewProject.jsx
@@ -1,16 +1,37 @@
+import { useRef } from "react";
 import Input from "./Input.jsx";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDesciption = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      desciption: enteredDesciption,
+      dueDate: enteredDueDate
+    })
+  }
+
   return (
-    <div>
-      <menu>
-        <li><button>Cancel</button></li>
-        <li><button>Save</button></li>
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+        </li>
+        <li>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950" onClick={handleSave}>Save</button>
+        </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+        <Input ref={title} label="Title" />
+        <Input ref={description} label="Description" textarea />
+        <Input ref={dueDate} label="Due Date" type="date" />
       </div>
     </div>
   );
