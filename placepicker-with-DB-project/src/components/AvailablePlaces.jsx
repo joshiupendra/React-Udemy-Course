dc1a284831c5a5002300a4e636d5cf773740dbfd
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-20 10:16:39 +0530

    Adding loading state while fetching places data

--- placepicker-with-DB-project/src/components/AvailablePlaces.jsx
+++ placepicker-with-DB-project/src/components/AvailablePlaces.jsx
@@ -3,13 +3,15 @@ import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPlaces() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
-
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -26,6 +28,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading = {isFetching}
+      loadingText = "Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
