d5e1b2964b1a898678ee68d4c7ff5562a974c42e
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-20 10:06:57 +0530

    Fetching Places

--- placepicker-with-DB-project/src/components/AvailablePlaces.jsx
+++ placepicker-with-DB-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,31 @@
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const resData = await response.json();
+      setAvailablePlaces(resData.places);
+
+    }
+
+    fetchPlaces();
+
+    /* // Can't use Async/ Await here as React doesn't allow async on react functional component
+    fetch("http://localhost:3000/places").then((response) => {
+      return response.json();
+    }).then((resData) => {
+      setAvailablePlaces(resData.places);
+    }); */
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
