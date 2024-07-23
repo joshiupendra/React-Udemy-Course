a6f2f6fe92557eeb832b1d87fe16c23bd69106f1
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-23 13:31:33 +0530

    Sent cart data into firebase

--- redux-cart-project/src/App.js
+++ redux-cart-project/src/App.js
@@ -1,18 +1,65 @@
-import { useSelector } from 'react-redux';
-
+import { useEffect, Fragment } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { uiActions } from './store/ui-slice';
+
+let isInitial = true;
 
 function App() {
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification);
 
-  return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
-  );
+  useEffect(() => {
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+        status: "pending",
+        title: "Sending",
+        message: "Sending cart data!"
+      }));
+      const response = await fetch("https://react-3a16d-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json", {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      dispatch(uiActions.showNotification({
+        status: "success",
+        title: "Success...",
+        message: "Sent cart data successfully!"
+      }));
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch(error => {
+      dispatch(uiActions.showNotification({
+        status: "error",
+        title: "Error!",
+        message: "Sending cart data failed!"
+      }));
+    });
+  }, [cart, dispatch]);
+
+return (
+  <Fragment>
+    {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+  <Layout>
+    {cartIsVisible && <Cart />}
+    <Products />
+  </Layout>
+  </Fragment>
+);
 }
 
 export default App;
