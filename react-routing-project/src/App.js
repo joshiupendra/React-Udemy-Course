b1fbec9d6c7ed90d486329d875418b7ea70eb01a
Upendra Joshi <joshiupendra9@gmail.com>
2024-07-23 22:13:17 +0530

    Adding nested routing and styling

--- react-routing-project/src/App.js
+++ react-routing-project/src/App.js
@@ -1,11 +1,18 @@
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 // Object Based Approach
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/products", element: <ProductsPage /> }
+  { 
+    path: "/", 
+    element: <RootLayout />, 
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> }
+    ] 
+  }, 
 ]);
 
 // Another Router Approach
--- react-routing-project/src/pages/Home.js
+++ react-routing-project/src/pages/Home.js
@@ -1,3 +1,10 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
-  return <h1>My Home Page</h1>
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <>Go to <Link to="/products">The Products Page</Link></>
+    </>
+  );
 }
